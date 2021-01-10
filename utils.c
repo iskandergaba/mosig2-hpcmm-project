c4a3b051ce9a612055487816fc1d187faf3836c6
Hadi Dayekh <hadi.dayekh@grenoble-inp.org>
2021-01-10 14:50:50 +0100

    print obstacle with *

--- utils.c
+++ utils.c
@@ -13,7 +13,10 @@ int cmpfunc_pos_y(const boid_t* a, const boid_t* b) {
 }
 
 void print_boid_pos(boid_t* b) {
-  printf("(%.2f\t %.2f)", b->pos_x, b->pos_y);
+  if (b->type == 0)
+    printf("(%.2f\t %.2f)*", b->pos_x, b->pos_y);
+  else
+    printf("(%.2f\t %.2f)", b->pos_x, b->pos_y);
 }
 
 void print_boid_grid(boid_t* grid, int d) {
