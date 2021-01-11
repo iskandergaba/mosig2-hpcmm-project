fbaa39ec9903b548d84b904e64fdd56f670c4992
Iskander Gaba <iskandergaba@gmail.com>
2021-01-11 11:04:48 +0100

    Added useful comments to the header files

--- utils.h
+++ utils.h
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct boid {
+typedef struct boid
+{
+  // position vector
   float pos_x;
   float pos_y;
+  // velocity vector
   float vel_x;
   float vel_y;
+  // type: 0 if obstacle and 1 if boid
   int type;
 } boid_t;
 
-typedef struct vector {
+typedef struct vector
+{
   float x;
   float y;
 } vector_t;
 
+// Coordinate comparison functions
+int cmpfunc_pos_x(const boid_t *a, const boid_t *b);
+int cmpfunc_pos_y(const boid_t *a, const boid_t *b);
 
-int cmpfunc_pos_x(const boid_t* a, const boid_t* b);
-int cmpfunc_pos_y(const boid_t* a, const boid_t* b);
-void print_boid_pos(boid_t* b);
-void print_boid_grid(boid_t* b, int d);
-void normalize_vector(vector_t* v);
+// Print the position of a boid
+void print_boid_pos(boid_t *b);
+// Print the grid of boids
+void print_boid_grid(boid_t *b, int d);
+// Normalize a vector
+void normalize_vector(vector_t *v);
